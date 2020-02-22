68c25ec8b62f11214d36ece1110510ce7a356304
Bill Nixon <bnixon67@gmail.com>
2020-02-22 16:48:01 -0600

    build to bin directory

--- examples/build.sh
+++ examples/build.sh
@@ -3,6 +3,7 @@
 for SRC in *.go
 do
 	echo ${SRC}
-	GOOS=linux GOARCH=amd64 go build ${SRC}
-	GOOS=windows GOARCH=amd64 go build ${SRC}
+	BASE=${SRC%.*}
+	GOOS=linux GOARCH=amd64 go build -o bin/${BASE} ${SRC}
+	GOOS=windows GOARCH=amd64 go build -o bin/${BASE}.exe ${SRC}
 done
