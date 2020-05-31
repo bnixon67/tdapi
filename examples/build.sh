33787b68895200c89c7f9373ae73df8240288c41
Bill Nixon <bnixon67@gmail.com>
2020-05-31 12:14:59 -0500

    Updated build to include arm

--- examples/build.sh
+++ examples/build.sh
@@ -4,6 +4,7 @@ for SRC in *.go
 do
 	echo ${SRC}
 	BASE=${SRC%.*}
-	GOOS=linux GOARCH=amd64 go build -o bin/${BASE} ${SRC}
+	GOOS=linux GOARCH=amd64 go build -o bin/${BASE}.amd64 ${SRC}
+	GOOS=linux GOARCH=arm go build -o bin/${BASE}.arm ${SRC}
 	GOOS=windows GOARCH=amd64 go build -o bin/${BASE}.exe ${SRC}
 done
