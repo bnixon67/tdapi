b166f9378f447056828a7867fa80915f99e08573
Bill Nixon <bnixon67@gmail.com>
2021-03-27 10:14:22 -0500

    added freebsd

--- examples/build.sh
+++ examples/build.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 for SRC in *.go
 do
 	echo ${SRC}
 	BASE=${SRC%.*}
-	GOOS=linux GOARCH=amd64 go build -o bin/${BASE}.amd64 ${SRC}
-	GOOS=linux GOARCH=arm go build -o bin/${BASE}.arm ${SRC}
+	GOOS=linux GOARCH=amd64 go build -o bin/${BASE}.linux.amd64 ${SRC}
+	GOOS=linux GOARCH=arm go build -o bin/${BASE}.linux.arm ${SRC}
+	GOOS=freebsd GOARCH=amd64 go build -o bin/${BASE}.freebsd.amd64 ${SRC}
 	GOOS=windows GOARCH=amd64 go build -o bin/${BASE}.exe ${SRC}
 done
